1a0a2409c7a47a3df498436f6e18fa5865b70bc1
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-02-12 15:51:06 +0100

    Added celsius symbol and margin to city card

--- week4/hackyourweather/src/components/Main.js
+++ week4/hackyourweather/src/components/Main.js
@@ -4,8 +4,8 @@ export const Main = ({ weatherData }) => {
   const { main, coord } = weatherData;
   return (
     <div className="main-details">
-      <p>min temp: {main.temp_min}</p>
-      <p>max temp: {main.temp_max}</p>
+      <p>min temp: {main.temp_min}°C</p>
+      <p>max temp: {main.temp_max}°C</p>
       <p>
         location: {coord.lon}, {coord.lat}
       </p>
